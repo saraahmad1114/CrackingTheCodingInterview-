1bbc38a0268201a413976516205d542f9436ff9f
Sara <saraahmad1114@yahoo.com>
2017-07-20 21:50:43 -0400

    first question done

--- Chapter 10: SORTING.playground/Contents.swift
+++ Chapter 10: SORTING.playground/Contents.swift
@@ -3,3 +3,59 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Chapter 10: SORTING 
+
+//1. You are given two sorted arrays, A and B, where A has a large enough buffer at the end to hold B. Write a method to merge B into A in sorted order. 
+
+//Classic Merge Sort Problem 
+
+func mergeSort(_ array: [Int]) -> [Int] {
+    guard array.count > 1 else { return array }
+    
+    let middleIndex = array.count / 2
+    
+    let leftArray = mergeSort(Array(array[0..<middleIndex]))
+    
+    let rightArray = mergeSort(Array(array[middleIndex..<array.count]))
+    
+    return merge(leftPile: leftArray, rightPile: rightArray)
+}
+
+func merge(leftPile: [Int], rightPile: [Int]) -> [Int] {
+    
+    var leftIndex = 0
+    var rightIndex = 0
+    
+   
+    var orderedPile = [Int]()
+    
+    
+    while leftIndex < leftPile.count && rightIndex < rightPile.count {
+        if leftPile[leftIndex] < rightPile[rightIndex] {
+            orderedPile.append(leftPile[leftIndex])
+            leftIndex += 1
+        } else if leftPile[leftIndex] > rightPile[rightIndex] {
+            orderedPile.append(rightPile[rightIndex])
+            rightIndex += 1
+        } else {
+            orderedPile.append(leftPile[leftIndex])
+            leftIndex += 1
+            orderedPile.append(rightPile[rightIndex])
+            rightIndex += 1
+        }
+    }
+    
+  
+    while leftIndex < leftPile.count {
+        orderedPile.append(leftPile[leftIndex])
+        leftIndex += 1
+    }
+    
+    while rightIndex < rightPile.count {
+        orderedPile.append(rightPile[rightIndex])
+        rightIndex += 1
+    }
+    
+    return orderedPile
+}
