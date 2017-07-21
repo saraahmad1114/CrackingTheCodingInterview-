7b08fe301d6d5f9914a9fcc711a5788c1478f7ca
Sara <saraahmad1114@yahoo.com>
2017-07-20 22:04:29 -0400

    done

--- Chapter 10: SORTING.playground/Contents.swift
+++ Chapter 10: SORTING.playground/Contents.swift
@@ -81,3 +81,5 @@ func findIndexOfElementIn(element: Int, array: [Int]) -> Int {
 
 //second method 
 
+
+//4. You are given an array-like data structure Listy which lacks a size method. It does, however, have an elementAt(i) method that returns the element at index i in O(1) time. If i is beyond the bounds of the data structure, it returns -1. (For this reason, the data structure only supports positive integers.) Given a Listy which contains sorted, positive integers, find the index at which an element x occurs. If x occurs multiple times, you may return any index. 
