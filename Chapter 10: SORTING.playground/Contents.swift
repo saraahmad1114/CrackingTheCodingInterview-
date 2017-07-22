3af96c908ad3d1a461caa53cf522cdd220f5f724
Sara <saraahmad1114@yahoo.com>
2017-07-21 23:37:20 -0400

    done

--- Chapter 10: SORTING.playground/Contents.swift
+++ Chapter 10: SORTING.playground/Contents.swift
@@ -58,6 +58,26 @@ func merge(leftPile: [Int], rightPile: [Int]) -> [Int] {
 
 //**Key = Anagram - are words that have different arrangements but same number of letters and the same letters 
 
+//create frequency dictionary / Can also create a hash table 
+
+func createFrequencyTable (word: String) -> [String: Int]{
+    var dictionary = [String:Int]()
+    for char in word.characters{
+        let stringChar = String(char)
+        if dictionary[stringChar] != nil {
+            dictionary[stringChar]! += 1
+        }
+        else {
+            dictionary[stringChar] = 1
+        }
+    }
+    print(dictionary)
+    return dictionary
+
+}
+
+createFrequencyTable(word: "hat")
+
 //3. Given a sorted array of n integers that has been rotated an unknown number of times, write code to find an element in the array. You may assume that the array was originally sorted in increasing order. 
 
 //input find: [15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14]
