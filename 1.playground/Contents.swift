b5e0d32e296412faf36b0792551a6e266e2d612d
Sara <saraahmad1114@yahoo.com>
2017-07-26 12:17:11 -0400

    done

--- 1.playground/Contents.swift
+++ 1.playground/Contents.swift
@@ -31,7 +31,7 @@ func areStringsPermutations (str1: String, str2: String) -> Bool{
     return frequencyDictionary(str1: str1) == frequencyDictionary(str1: str2)
 }
 
-//1.3 - Write a method to replace all spaces in a string with '%20  You may assume that the string has sufficient space at the end to hold the additional characters,and that you are given the "true" length of the string. (Note: If implementing in Java,please use a character array so that you can perform this operation in place.)
+//1.3 - Write a method to replace all spaces in a string with '%20  You may assume that the string has sufficient space at the end to hold the additional characters,and that you are given the "true" length of the string.
 
 //EXAMPLE
 //Input: "Mr John Smith ", 13 Output: "Mr%20John%20Smith"
@@ -43,6 +43,22 @@ func removeSpacesWith (str1: String) -> String {
     return newString
 
 }
+
+//1.4 - Given a string, write a function to check if it is a permutation of a palin­drome. A palindrome is a word or phrase that is the same  rwards and backwards. A permutation is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.
+
+func isStringPermutationOfPalindrome(str1: String, str2: String) -> Bool {
+
+    if String(str1.characters) == String(str2.characters.reversed()){
+        if frequencyDictionary(str1: str1) == frequencyDictionary(str1: str2){
+            return true
+        }
+    }
+    return false
+}
+
+//1.5 - There are three types of edits that can be performed on strings; insert a character, remove a character, or replace a character. Given two strings, write a function to check if they are one edit (or zero edits) away. 
+
+
 
 
 
