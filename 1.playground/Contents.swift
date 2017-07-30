506460b5eace82625b25ce619cc2bb856d2c3008
Sara <saraahmad1114@yahoo.com>
2017-07-30 18:59:12 -0400

    done 2

--- 1.playground/Contents.swift
+++ 1.playground/Contents.swift
@@ -256,6 +256,8 @@ func compress(input: String) -> String {
 
 //1.7 - Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
 
+
+
 //1.8 - Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column are set to 1.
 
 func markRowAndColumn(input: [[Int]]) -> [[Int]] {
