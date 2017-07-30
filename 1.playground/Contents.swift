489b97074298e65a47235e2f4d45d8bfd18fdc37
Sara <saraahmad1114@yahoo.com>
2017-07-30 18:09:45 -0400

    done

--- 1.playground/Contents.swift
+++ 1.playground/Contents.swift
@@ -174,12 +174,7 @@ isThereAReplacement(str1: "sara", str2: "zara")
 
 isThereAReplacement(str1: "sara", str2: "zero")
 
-//returns false 
-
-
-
-
-
+//returns false
 
 func editsTo (str1: String, str2: String) ->  (Bool, String){
 
@@ -203,8 +198,16 @@ func editsTo (str1: String, str2: String) ->  (Bool, String){
 
 editsTo(str1: "sale", str2: "sales")
 
-//returning false
+//returns true - Insertion
 
+
+editsTo(str1: "bake", str2: "bke")
+
+//returns true - Removal
+
+editsTo(str1: "bake", str2: "sake")
+
+//returns true - Replacement 
 
 //1.6 - Implement a method to peform basic string compression using the counts of repeated characters. For example, the string aabcccccaaa would become a2b1c5c3. If the "compressed" string would not become smaller than the original string, your method should return the original string. You can assume the string has only uppercase and lowercase (a - z).
 
