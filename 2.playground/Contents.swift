0520ba519da035e80245a18f38cef912c66484ec
Sara <saraahmad1114@yahoo.com>
2017-07-31 20:49:46 -0400

    done

--- 2.playground/Contents.swift
+++ 2.playground/Contents.swift
@@ -4,21 +4,4 @@ import UIKit
 
 var str = "Hello, playground"
 
-func findTheMax (array: [Int]) -> Int {
 
-    var maxVal = 0
-    var product = 1
-    var productArray = [Int]()
-    
-    for i in 0..<array.count {
-        for (index, value) in array.enumerated(){
-            product = array[i] * array[index] * value
-            productArray.append(product)
-        }
-    }
-    return productArray.max()!
-}
-
-var array = [6, 7, 9, 7, 10, 11, 16]
-
-findTheMax(array: array)
