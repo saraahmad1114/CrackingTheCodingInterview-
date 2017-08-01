23f155a0dc57da08ab6d529be5f1a983f343e779
Sara <saraahmad1114@yahoo.com>
2017-07-31 20:51:32 -0400

    added first question

--- Chapter 2.playground/Contents.swift
+++ Chapter 2.playground/Contents.swift
@@ -4,4 +4,12 @@ import UIKit
 
 var str = "Hello, playground"
 
+//Chapter 2 
+
+//2.1 - Remove Duplicates: Write code to remove duplicates from an unsorted linked list.
+//FOLLOW UP
+//How would you solve this problem if a temporary buffer is not allowed?
+
+
+
 
