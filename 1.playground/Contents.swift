2f917b1f7bf50fb381618bec1996d6f8e60c4cd2
Sara <saraahmad1114@yahoo.com>
2017-07-31 20:29:40 -0400

    added something

--- 1.playground/Contents.swift
+++ 1.playground/Contents.swift
@@ -317,6 +317,27 @@ isSubstring(str1: "erbottlewat")
 //returns true 
 
 
+func findTheMax (array: [Int]) -> Int {
+    
+    var maxVal = 0
+    var product = 1
+    var productArray = [Int]()
+    
+    for i in 0..<array.count {
+        for (index, value) in array.enumerated(){
+            product = array[i] * array[index] * value
+            productArray.append(product)
+        }
+    }
+    return productArray.max()!
+}
+
+var array = [6, 7, 9, 7, 10, 11, 16]
+
+findTheMax(array: array)
+
+
+
 
 
 
