1601c5106ea8bc83744d2b8eb0c30624e1e2c3fc
Sara <saraahmad1114@yahoo.com>
2017-08-05 21:01:39 -0400

    added question

--- Chapter 2.playground/Contents.swift
+++ Chapter 2.playground/Contents.swift
@@ -66,7 +66,7 @@ print(removeDups(head: h))
 
 //2.3 - Implement an algorithm to delete a node in the middle (i.e., any node but the  rst and last node, not necessarily the exact middle) of a singly linked list, given only access to that node.
 
-
+//2.4 - Write code to partition a linked list around a value x, such that all nodes less than x come before all nodes greater than or equal to x. If x is contained within the list, the values of x only need to be after the elements less than x (see below). The partition element x can appear anywhere in the "right partition"; it does not need to appear between the left and right partitions.
 
 
 
