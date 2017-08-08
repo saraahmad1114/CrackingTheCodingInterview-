41b5b8fd4f9fbb13ab4ae72ba1e12922471d9317
Sara <saraahmad1114@yahoo.com>
2017-08-08 19:19:54 -0400

    done

--- Chapter2.3.playground/Contents.swift
+++ Chapter2.3.playground/Contents.swift
@@ -4,6 +4,12 @@ import UIKit
 
 var str = "Hello, playground"
 
-//2.3 - Implement an algorithm to delete a node in the middle (i.e., any node but the  rst and last node, not necessarily the exact middle) of a singly linked list, given only access to that node.
+//2.3 -  Implement an algorithm to delete a node in the middle (i.e., any node but the  rst and last node, not necessarily the exact middle) of a singly linked list, given only access to that node.
+
+//EXAMPLE
+//lnput:the node c from the linked lista->b->c->d->e->f
+//Result: nothing is returned, but the new linked list looks likea->b->d->e- >f
+
+
 
 
