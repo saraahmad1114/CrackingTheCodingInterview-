b49a3d1d19b49efb367765e402c865103fc85f72
Sara <saraahmad1114@yahoo.com>
2017-08-08 19:18:33 -0400

    added file

---
+++ Chapter2.3.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//2.3 - Implement an algorithm to delete a node in the middle (i.e., any node but the  rst and last node, not necessarily the exact middle) of a singly linked list, given only access to that node.
+
+
