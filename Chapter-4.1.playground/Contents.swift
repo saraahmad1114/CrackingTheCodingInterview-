c02e48b1ac02d34333e7e2c0597f45ee1b72fdc1
Sara <saraahmad1114@yahoo.com>
2017-08-24 15:09:22 -0400

    added

--- Chapter-4.1.playground/Contents.swift
+++ Chapter-4.1.playground/Contents.swift
@@ -7,3 +7,8 @@ var str = "Hello, playground"
 //Chapter 4 
 
 //4.1 - Given a directed graph, design an algorithm to  nd out whether there is a route between two nodes.
+
+
+//4.2 - Given a sorted (increasing order) array with unique integer elements, write an algo­rithm to create a binary search tree with minimal height
+
+//
