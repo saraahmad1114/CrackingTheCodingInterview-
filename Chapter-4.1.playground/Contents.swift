a3b633b50b00ae151cffb63288892d9efedebf82
Sara <saraahmad1114@yahoo.com>
2017-08-28 22:01:06 -0400

    solution

--- Chapter-4.1.playground/Contents.swift
+++ Chapter-4.1.playground/Contents.swift
@@ -16,3 +16,75 @@ var str = "Hello, playground"
 //4.3 - Given a binary tree, design an algorithm which creates a linked list of all the nodes at each depth (e.g., if you have a tree with depth D, you'll have D linked lists).
 
 
+
+
+extension Int {
+    var wordForm:String {
+        let numberValue = NSNumber(value: self)
+        var formatter = NumberFormatter()
+        formatter.numberStyle = .spellOut
+        return "\(formatter.string(for: self)!)"
+    }
+}
+
+func printCharNum(number: Int) -> Int{
+    
+    var numberWordCount = 0
+    
+    var containerString = number.wordForm
+    
+    containerString = containerString.replacingOccurrences(of: " ", with: "")
+    
+    if containerString.characters.contains("-"){
+        containerString = containerString.replacingOccurrences(of: "-", with: "")
+        print(containerString)
+        return containerString.characters.count
+    }
+    print(containerString)
+    return containerString.characters.count
+    
+}
+
+//printCharNum(number: 178)
+
+//Another way would be to do the following: if you insist on using the loop
+
+func printCharactersOfNumbers(givenNum: Int) -> Int {
+
+    var containerString = ""
+    var secondContainer = ""
+    var dictionary = [Int: String]()
+    
+    var counter = 0
+    
+    for number in 1...1000 {
+        
+        containerString = number.wordForm.replacingOccurrences(of: " ", with: "")
+        
+        if dictionary[number] == nil {
+            dictionary[number] =  containerString
+        }
+    }
+    
+    print(dictionary)
+  
+    for (key, value) in dictionary {
+        
+        if key == givenNum {
+            secondContainer = dictionary[key]!
+        }
+    }
+    
+    secondContainer = secondContainer.replacingOccurrences(of: " ", with: "")
+    if secondContainer.characters.contains("-"){
+        secondContainer = secondContainer.replacingOccurrences(of: "-", with: "")
+        print(secondContainer)
+        return secondContainer.characters.count
+    }
+    print(secondContainer)
+    return secondContainer.characters.count
+}
+
+printCharactersOfNumbers(givenNum: 750)
+
+
