2f7d03cbad3220ec093ee2b3f4b001b9260727de
Sara <saraahmad1114@yahoo.com>
2017-08-29 22:46:27 -0400

    first

--- Chapter-4.1.playground/Contents.swift
+++ Chapter-4.1.playground/Contents.swift
@@ -8,7 +8,34 @@ var str = "Hello, playground"
 
 //4.1 - Given a directed graph, design an algorithm to find out whether there is a route between two nodes.
 
-//1. Define the graph 
+//1. Define the graph
+
+public struct Vertex<T: Hashable> {
+    var data: T
+}
+
+extension Vertex: Hashable {
+    public var hashValue: Int { // 1
+        return "\(data)".hashValue
+    }
+    
+    static public func ==(lhs: Vertex, rhs: Vertex) -> Bool { // 2
+        return lhs.data == rhs.data
+    }
+}
+
+extension Vertex: CustomStringConvertible {
+    public var description: String {
+        return "\(data)"
+    }
+}
+
+enum state {
+
+    case Unvisited
+    case Visited
+    case Visiting
+}
 
 
 
