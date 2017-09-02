3bd34209f5afdb43a0301d6fd65876c0f145ac51
Sara <saraahmad1114@yahoo.com>
2017-09-02 15:06:21 -0400

    added

---
+++ Chapter-4.2.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//4.2 Question: Given a sorted (increasing order) array with unique integer elements, write an algo­rithm to create a binary search tree with minimal height.
+
+
