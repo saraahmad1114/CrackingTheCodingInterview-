d62b3a3872d6163493aa7cd02fbaefe19abc037c
Sara <saraahmad1114@yahoo.com>
2017-09-18 23:52:38 -0400

    added questiong

--- Chapter-5.1.playground/Contents.swift
+++ Chapter-5.1.playground/Contents.swift
@@ -3,3 +3,7 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//5.1 - You are given two 32-bit numbers, N and M, and two bit positions, i and j. Write a method to insert M into N such that M starts at bit j and ends at bit i. You can assume that the bits j through i have enough space to  t all of M. That is, if M = 10011, you can assume that there are at least 5 bits between j and i. You would not,  r example, have j = 3 and i = 2, because M could not fully  t between bit 3 and bit 2
+
+//
