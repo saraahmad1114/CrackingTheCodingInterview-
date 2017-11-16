acf07dfd35e89b36d67b8ef0af89064a6103262f
saraahmad1114 <asara1114@gmail.com>
2017-11-16 18:00:33 -0500

    done

--- LeetCode3.playground/Contents.swift
+++ LeetCode3.playground/Contents.swift
@@ -13,3 +13,24 @@ var str = "Hello, playground"
 //Given "bbbbb", the answer is "b", with the length of 1.
 
 //Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+func lengthOfLongestSubstring(string: String) -> Int {
+    var substringChars: [Character] = []
+    var longest = 0
+    
+    for c in string {
+        if !substringChars.contains(c) {
+            substringChars.append(c)
+        } else {
+            longest = max(longest, substringChars.count)
+            substringChars = [c]
+        }
+    }
+    longest = max(longest, substringChars.count)
+    return longest
+}
+
+lengthOfLongestSubstring(string: "abcabcbb")
+
+lengthOfLongestSubstring(string: "bbbbb")
+
